0b65fe82ab2ce5f71032a843145c81b21013c6cb
Gigas002 <gigas002@yandex.ru>
2019-09-06 23:51:54 +0300

    Various performance improvements in program.cs

--- DoujinDownloader/Program.cs
+++ DoujinDownloader/Program.cs
@@ -8,6 +8,8 @@ using CommandLine;
 using Newtonsoft.Json;
 using DoujinDownloader.Enums;
 
+//TODO: Localization
+
 namespace DoujinDownloader
 {
     internal static class Program
@@ -54,29 +56,47 @@ namespace DoujinDownloader
 
             stopwatch.Start();
 
-            Doujins doujins = new Doujins();
+            try { await StartWork().ConfigureAwait(false); }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+
+                #if DEBUG
+                Console.WriteLine(exception.InnerException?.Message);
+                #endif
+
+                return;
+            }
+
+            //After work is done.
+            stopwatch.Stop();
+            Console.WriteLine($"Done in hours:{stopwatch.Elapsed.Hours}, " +
+                              $"minutes:{stopwatch.Elapsed.Minutes}, seconds:{stopwatch.Elapsed.Seconds}, " +
+                              $"milliseconds:{stopwatch.Elapsed.Milliseconds}.");
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// Makes all the work.
+        /// </summary>
+        /// <returns></returns>
+        private static async ValueTask StartWork()
+        {
+            Doujins doujins = null;
 
             //If input file extensions .json => deserialize it to Doujins object.
             if (InputFileInfo.Extension == Extensions.JsonExtension)
             {
-                try
-                {
-                    doujins = JsonConvert.DeserializeObject<Doujins>(await File.ReadAllTextAsync(InputFileInfo.FullName)
-                                                                               .ConfigureAwait(false));
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine($"Unable to deserialize selected {Extensions.JsonExtension} file.");
-
-                    return;
-                }
-
+                doujins = JsonConvert.DeserializeObject<Doujins>(await File.ReadAllTextAsync(InputFileInfo.FullName)
+                                                                           .ConfigureAwait(false));
                 JsonFileInfo = InputFileInfo;
             }
             //Else if this is .md file => parse it to Doujins object and write to .json file.
             else if (InputFileInfo.Extension == Extensions.MarkdownExtension)
             {
-                doujins = await MarkdownParser.ParseMarkdownAsync(InputFileInfo.FullName, ArtistName).ConfigureAwait(false);
+                doujins = await MarkdownParser.ParseMarkdownAsync(InputFileInfo.FullName, ArtistName)
+                                              .ConfigureAwait(false);
 
                 //If no doujins in .md file.
                 if (!doujins.DoujinsList.Any())
@@ -87,29 +107,21 @@ namespace DoujinDownloader
                 }
 
                 //Write to .json
-                await Task.Run(() => WriteJson(doujins)).ConfigureAwait(false);
+                await WriteJsonAsync(doujins).ConfigureAwait(false);
             }
 
             //Print some additional info.
-            await Task.Run(() => PrintCount(doujins)).ConfigureAwait(false);
+            await PrintCountAsync(doujins).ConfigureAwait(false);
 
             //Test files names => try to write on disk.
-            await Task.Run(() => TestDoujinsNames(doujins, Paths.DoujinsDirectoryPath)).ConfigureAwait(false);
+            await TestDoujinsNamesAsync(doujins, Paths.DoujinsDirectoryPath).ConfigureAwait(false);
 
             //Write uris to use in HitomiDownloader (for example).
-            await Task.Run(() => WriteUrisAsync(doujins, $"{UrisFileInfo.FullName}")).ConfigureAwait(false);
+            await WriteUrisAsync(doujins, $"{UrisFileInfo.FullName}").ConfigureAwait(false);
 
             //TODO
             //Download doujin.
-
-            //After work is done.
-            stopwatch.Stop();
-            Console.WriteLine($"Done in hours:{stopwatch.Elapsed.Hours}, " +
-                              $"minutes:{stopwatch.Elapsed.Minutes}, seconds:{stopwatch.Elapsed.Seconds}, " +
-                              $"milliseconds:{stopwatch.Elapsed.Milliseconds}.");
         }
-
-        #region Methods
 
         /// <summary>
         /// Set properties values from command line options.
@@ -160,90 +172,74 @@ namespace DoujinDownloader
         /// Writes .json file from <see cref="Doujins"/> object.
         /// </summary>
         /// <param name="doujins">Object with <see cref="Doujin"/> list.</param>
-        private static void WriteJson(Doujins doujins)
+        private static async ValueTask WriteJsonAsync(Doujins doujins) => await Task.Run(async () =>
         {
             JsonFileInfo.Directory?.Create();
 
-            using (StreamWriter streamWriter = JsonFileInfo.CreateText())
-            {
-                JsonSerializer jsonSerializer = new JsonSerializer();
-                jsonSerializer.Serialize(streamWriter, doujins);
-            }
+            await using StreamWriter streamWriter = JsonFileInfo.CreateText();
+            JsonSerializer jsonSerializer = new JsonSerializer();
+            jsonSerializer.Serialize(streamWriter, doujins);
 
             JsonFileInfo.Refresh();
-        }
+        }).ConfigureAwait(false);
 
         /// <summary>
         /// Prints count of doujins list.
         /// </summary>
         /// <param name="doujins">Object with <see cref="Doujin"/> list.</param>
-        private static void PrintCount(Doujins doujins)
+        private static async ValueTask PrintCountAsync(Doujins doujins) => await Task.Run(() =>
         {
             Console.WriteLine($"Doujins count:{doujins.DoujinsList.Count}");
             Console.WriteLine($"Doujins to download count:{GetDoujinsToDownload(doujins).Count()}");
-        }
+        }).ConfigureAwait(false);
 
         /// <summary>
         /// Test method for writing test .txt files from doujins list on disk.
         /// </summary>
         /// <param name="doujins">Object with <see cref="Doujin"/> list.</param>
         /// <param name="doujinsDirectoryPath">Directory for doujin files.</param>
-        private static void TestDoujinsNames(Doujins doujins, string doujinsDirectoryPath)
-        {
-            #if RELEASE
-            return;
-            #endif
-
-            foreach (Doujin doujin in doujins.DoujinsList)
+        private static async ValueTask TestDoujinsNamesAsync(Doujins doujins, string doujinsDirectoryPath) =>
+            await Task.Run(() =>
             {
-                string subsection = string.IsNullOrWhiteSpace(doujin.Subsection) ? string.Empty : doujin.Subsection;
-                string language = string.IsNullOrWhiteSpace(doujin.Language) ? string.Empty : doujin.Language;
+                #if RELEASE
+                return;
+                #endif
 
-                string path = Path.Combine(doujinsDirectoryPath, doujin.Artist, subsection, doujin.Name, language,
-                                           $"{doujin.Name}{Extensions.TxtExtension}");
-                FileInfo doujinFileInfo = new FileInfo(path);
-
-                try
+                Parallel.ForEach(doujins.DoujinsList, doujin =>
                 {
+                    string subsection = string.IsNullOrWhiteSpace(doujin.Subsection) ? string.Empty : doujin.Subsection;
+                    string language = string.IsNullOrWhiteSpace(doujin.Language) ? string.Empty : doujin.Language;
+
+                    string path = Path.Combine(doujinsDirectoryPath, doujin.Artist, subsection, doujin.Name, language,
+                                               $"{doujin.Name}{Extensions.TxtExtension}");
+                    FileInfo doujinFileInfo = new FileInfo(path);
+
                     doujinFileInfo.Directory?.Create();
                     doujinFileInfo.Create();
-                }
-                catch (Exception) { Console.WriteLine($"Wrong path:{path}"); }
-            }
-        }
+                });
+            }).ConfigureAwait(false);
 
         /// <summary>
         /// Writes only all uris to download from <see cref="Doujins"/> object to text file.
         /// </summary>
         /// <param name="doujins">Object with <see cref="Doujin"/> list.</param>
         /// <param name="urisTxtPath">Full path to uris.txt file..</param>
-        private static async ValueTask WriteUrisAsync(Doujins doujins, string urisTxtPath)
+        private static async ValueTask WriteUrisAsync(Doujins doujins, string urisTxtPath) => await Task.Run(async () =>
         {
             List<Doujin> doujinsList = GetDoujinsToDownload(doujins).ToList();
 
-            try
-            {
-                FileInfo txtFileInfo = new FileInfo(urisTxtPath);
-                if (txtFileInfo.Exists) txtFileInfo.Delete();
-                txtFileInfo.Directory?.Create();
-            }
-            catch (Exception)
-            {
-                Console.WriteLine($"Unable to create directory:{urisTxtPath} or delete file:{urisTxtPath}.");
-            }
+            FileInfo txtFileInfo = new FileInfo(urisTxtPath);
+            if (txtFileInfo.Exists) txtFileInfo.Delete();
+            txtFileInfo.Directory?.Create();
 
-            for (int i = 0; i < doujinsList.Count; i++)
+            for (int index = 0; index < doujinsList.Count; index++)
             {
-                try
-                {
-                    string uriToAppend = i == doujinsList.Count - 1
-                                             ? $"{doujinsList[i].Uri}"
-                                             : $"{doujinsList[i].Uri}, ";
-                    await File.AppendAllTextAsync(urisTxtPath, uriToAppend).ConfigureAwait(false);
-                }
-                catch (Exception) { Console.WriteLine($"Unable to append uri {doujinsList[i].Uri} to file."); }
+                string uriToAppend = index == doujinsList.Count - 1
+                                         ? $"{doujinsList[index].Uri}"
+                                         : $"{doujinsList[index].Uri}, ";
+                await File.AppendAllTextAsync(urisTxtPath, uriToAppend).ConfigureAwait(false);
             }
-        }
+        }).ConfigureAwait(false);
 
         /// <summary>
         /// Gets list of doujins to download.
@@ -251,8 +247,7 @@ namespace DoujinDownloader
         /// <param name="doujins">Object with <see cref="Doujin"/> list.</param>
         /// <returns>List of <see cref="Doujin"/> objects.</returns>
         private static IEnumerable<Doujin> GetDoujinsToDownload(Doujins doujins) =>
-            //TODO: test AsParallel and IAsyncEnumerable
-            doujins.DoujinsList.Where(doujin => doujin.Uri != null && !doujin.IsDownloaded);
+            doujins.DoujinsList.Where(doujin => doujin.Uri != null && !doujin.IsDownloaded).AsParallel();
 
         #endregion
     }
