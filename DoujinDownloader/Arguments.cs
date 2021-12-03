a54095c0affd8842dc16718c9cc7b9bf316021ac
gigas002 <gigas002@yandex.ru>
2021-12-03 14:28:56 +0300

    Rewrite Program and Arguments classes for new structure

--- DoujinDownloader/Arguments.cs
+++ DoujinDownloader/Arguments.cs
@@ -9,29 +9,25 @@ namespace DoujinDownloader;
 /// </summary>
 public class Arguments
 {
-    #region Required
+    #region Optional
 
     /// <summary>
     /// Full path to input file.
     /// </summary>
-    [Option('i', "input", Required = true, HelpText = "Full path to input file.")]
-    public string InputFilePath { get; set; }
-
-    #endregion
-
-    #region Optional
+    [Option('i', "input", Required = false, HelpText = "Full path to input file")]
+    public string InputPath { get; set; } = "doujins.md";
 
     /// <summary>
     /// Path to converted json.
     /// </summary>
-    [Option('j', "json", Required = false, HelpText = "Path to converted json.")]
-    public string JsonPath { get; set; } = "Doujins.json";
+    [Option('j', "json", Required = false, HelpText = "Path to converted json")]
+    public string JsonPath { get; set; } = "doujins.json";
 
     /// <summary>
     /// Path to .txt file with raw uris, to use in HitomiDownloader.
     /// </summary>
-    [Option('u', "uris", Required = false, HelpText = "Path to .txt file with raw uris, to use in HitomiDownloader.")]
-    public string UrisPath { get; set; } = "Uris.txt";
+    [Option('u', "urls", Required = false, HelpText = "Path to .txt file with raw uris")]
+    public string UrisPath { get; set; } = "urls.txt";
 
     #endregion
 }
--- DoujinDownloader/Program.cs
+++ DoujinDownloader/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Diagnostics;
 using System.Text;
-using CommandLine;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Json.Serialization;
+using CommandLine;
 using DoujinDownloader.Constants;
 using DoujinDownloader.Localization;
 
@@ -14,46 +16,54 @@ internal static class Program
     /// <summary>
     /// .md or .json file with doujin list.
     /// </summary>
-    private static FileInfo InputFileInfo { get; set; }
+    private static string InputPath { get; set; }
 
     /// <summary>
     /// Converted .json (if input was .md).
     /// </summary>
-    private static FileInfo JsonFileInfo { get; set; }
+    private static string JsonPath { get; set; }
 
     /// <summary>
     /// .txt file with raw uris, to use in (for example) HitomiDownloader.
     /// </summary>
-    private static FileInfo UrisFileInfo { get; set; }
+    private static string UrlsPath { get; set; }
 
     /// <summary>
     /// Shows if there were command line parsing errors.
     /// </summary>
     private static bool IsParsingErrors { get; set; }
 
+    public static JsonSerializerOptions JsonOptions { get; } = new()
+    {
+        Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+    };
+
     #endregion
 
-    internal static async Task Main(string[] args)
+    internal static void Main(string[] args)
     {
-        Stopwatch stopwatch = new Stopwatch();
+        var stopwatch = new Stopwatch();
 
         //Get command line args
-        Parser.Default.ParseArguments<Arguments>(args).WithParsed(ParseConsoleOptions)
-              .WithNotParsed(error => IsParsingErrors = true);
+        Parser.Default.ParseArguments<Arguments>(args).WithParsed(ParseConsoleOptions).WithNotParsed(_ => IsParsingErrors = true);
 
         //Stop executing if errors occured
         if (IsParsingErrors) return;
 
         stopwatch.Start();
 
-        try { await StartWork().ConfigureAwait(false); }
+        try
+        {
+            ParseAndDownloadDoujins();
+        }
         catch (Exception exception)
         {
             Console.WriteLine(exception.Message);
 
-#if DEBUG
+            #if DEBUG
             Console.WriteLine(exception.InnerException?.Message);
-#endif
+            #endif
 
             return;
         }
@@ -68,11 +78,11 @@ internal static class Program
     /// <summary>
     /// Set properties values from command line options
     /// </summary>
-    /// <param name="options">Command line options</param>
-    private static void ParseConsoleOptions(Arguments options)
+    /// <param name="arguments">Command line args</param>
+    private static void ParseConsoleOptions(Arguments arguments)
     {
-        //Check if string options are empty strings.
-        if (string.IsNullOrWhiteSpace(options.InputFilePath))
+        // Check if string options are empty strings
+        if (string.IsNullOrWhiteSpace(arguments.InputPath))
         {
             Console.WriteLine(Strings.InputNotSet);
             IsParsingErrors = true;
@@ -80,12 +90,12 @@ internal static class Program
             return;
         }
 
-        //Set properties values.
-        InputFileInfo = new FileInfo(options.InputFilePath);
-        JsonFileInfo = new FileInfo(options.JsonPath);
-        UrisFileInfo = new FileInfo(options.UrisPath);
+        // Set properties values
+        InputPath = arguments.InputPath;
+        JsonPath = arguments.JsonPath;
+        UrlsPath = arguments.UrisPath;
 
-        if (JsonFileInfo.Extension != Extensions.JsonExtension)
+        if (Path.GetExtension(JsonPath) != Extensions.JsonExtension)
         {
             Console.WriteLine(Strings.JsonWrongExtension, Extensions.JsonExtension);
             IsParsingErrors = true;
@@ -93,7 +103,7 @@ internal static class Program
             return;
         }
 
-        if (UrisFileInfo.Extension != Extensions.TxtExtension)
+        if (Path.GetExtension(UrlsPath) != Extensions.TxtExtension)
         {
             Console.WriteLine(Strings.UrisWrongExtension, Extensions.TxtExtension);
             IsParsingErrors = true;
@@ -101,128 +111,100 @@ internal static class Program
             return;
         }
 
-        if (InputFileInfo.Extension == Extensions.JsonExtension ||
-            InputFileInfo.Extension == Extensions.MarkdownExtension)
+        if (Path.GetExtension(InputPath) == Extensions.JsonExtension ||
+            Path.GetExtension(InputPath) == Extensions.MarkdownExtension)
             return;
 
         Console.WriteLine(Strings.InputWrongExtension, Extensions.JsonExtension, Extensions.MarkdownExtension);
         IsParsingErrors = true;
     }
 
-    /// <summary>
-    /// Makes all the work async
-    /// </summary>
-    /// <returns></returns>
-    private static async ValueTask StartWork()
+    private static void ParseAndDownloadDoujins()
     {
-        Doujins doujins;
+        List<Doujin> doujins;
 
-        switch (InputFileInfo.Extension)
+        var extension = Path.GetExtension(InputPath);
+
+        switch (extension)
         {
             case Extensions.JsonExtension:
+            {
+                using var fs = File.OpenRead(InputPath);
+                doujins = JsonSerializer.Deserialize<IEnumerable<Doujin>>(fs, JsonOptions).ToList();
+
+                // If no doujins in .json file
+                if (!doujins.Any())
                 {
-                    doujins = await JsonSerializer.DeserializeAsync<Doujins>(InputFileInfo.OpenRead())
-                                                  .ConfigureAwait(false) ?? new Doujins();
-
-                    //If no doujins in .json file.
-                    if (!doujins.DoujinsList.Any())
-                    {
-                        Console.WriteLine(Strings.NoDoujinsInInput, Extensions.JsonExtension);
-
-                        return;
-                    }
-
-                    JsonFileInfo = InputFileInfo;
-
-                    break;
-                }
-            case Extensions.MarkdownExtension:
-                {
-                    doujins = await MarkdownParser.ParseMarkdownAsync(InputFileInfo.FullName).ConfigureAwait(false);
-
-                    //If no doujins in .md file.
-                    if (!doujins.DoujinsList.Any())
-                    {
-                        Console.WriteLine(Strings.NoDoujinsInInput, Extensions.MarkdownExtension);
-
-                        return;
-                    }
-
-                    //Write to .json
-                    await WriteJsonAsync(doujins).ConfigureAwait(false);
-
-                    break;
-                }
-            default:
-                {
-                    Console.WriteLine(Strings.NotSupported, InputFileInfo.Extension);
+                    Console.WriteLine(Strings.NoDoujinsInInput, Extensions.JsonExtension);
 
                     return;
                 }
+
+                JsonPath = InputPath;
+
+                break;
+            }
+            case Extensions.MarkdownExtension:
+            {
+                var mdLines = File.ReadAllLines(InputPath);
+                doujins = MarkdownParser.ParseMarkdownAsync(mdLines).ToList();
+
+                // If no doujins in .md file
+                if (!doujins.Any())
+                {
+                    Console.WriteLine(Strings.NoDoujinsInInput, Extensions.MarkdownExtension);
+
+                    return;
+                }
+
+                // Write to .json
+                using var fs = File.OpenWrite(JsonPath);
+                JsonSerializer.Serialize(fs, doujins, JsonOptions);
+
+                break;
+            }
+            default:
+            {
+                Console.WriteLine(Strings.NotSupported, extension);
+
+                return;
+            }
         }
 
-        //Print some additional info
-        await PrintCountAsync(doujins).ConfigureAwait(false);
+        // Print some additional info
+        PrintCount(doujins);
 
-        //Write uris to use in HitomiDownloader (for example)
-        await WriteUrisAsync(doujins, $"{UrisFileInfo.FullName}").ConfigureAwait(false);
+        // Write urls file
+        WriteUrls(doujins, $"{UrlsPath}");
 
-        //TODO
-        //Download doujins
+        //TODO: Download doujins through gallery-dl
     }
 
-    /// <summary>
-    /// Writes .json file from <see cref="Doujins"/> object
-    /// </summary>
-    /// <param name="doujins">Object with <see cref="Doujin"/> list</param>
-    private static async ValueTask WriteJsonAsync(Doujins doujins) => await Task.Run(async () =>
+    private static void PrintCount(IEnumerable<Doujin> doujins)
     {
-        JsonFileInfo.Directory?.Create();
+        var djs = doujins.ToList();
 
-        await using FileStream fileStream = JsonFileInfo.OpenWrite();
-        await JsonSerializer.SerializeAsync(fileStream, doujins).ConfigureAwait(false);
+        Console.WriteLine(Strings.DoujinsCount, djs.Count);
+        Console.WriteLine(Strings.DoujinsToDownloadCount, GetDoujinsToDownload(djs).Count());
+    }
 
-        JsonFileInfo.Refresh();
-    }).ConfigureAwait(false);
-
-    /// <summary>
-    /// Prints count of doujins list and doujins to download
-    /// </summary>
-    /// <param name="doujins">Object with <see cref="Doujin"/> list</param>
-    private static async ValueTask PrintCountAsync(Doujins doujins) => await Task.Run(() =>
+    private static void WriteUrls(IEnumerable<Doujin> doujins, string urlsTxtPath)
     {
-        Console.WriteLine(Strings.DoujinsCount, doujins.DoujinsList.Count);
-        Console.WriteLine(Strings.DoujinsToDownloadCount, GetDoujinsToDownload(doujins).Count());
-    }).ConfigureAwait(false);
+        var doujinsList = GetDoujinsToDownload(doujins).ToList();
 
-    /// <summary>
-    /// Writes all uris to download from <see cref="Doujins"/> object to text file
-    /// </summary>
-    /// <param name="doujins">Object with <see cref="Doujin"/> list</param>
-    /// <param name="urisTxtPath">Full path to uris.txt file</param>
-    private static async ValueTask WriteUrisAsync(Doujins doujins, string urisTxtPath)
-    {
-        List<Doujin> doujinsList = GetDoujinsToDownload(doujins).ToList();
+        if (File.Exists(urlsTxtPath)) File.Delete(urlsTxtPath);
+        // txtFileInfo.Directory?.Create();
+        using var fs = File.OpenWrite(urlsTxtPath);
 
-        FileInfo txtFileInfo = new FileInfo(urisTxtPath);
-        if (txtFileInfo.Exists) txtFileInfo.Delete();
-        txtFileInfo.Directory?.Create();
-        await using FileStream fileStream = txtFileInfo.OpenWrite();
-
-        foreach (Doujin doujin in doujinsList)
+        foreach (var doujin in doujinsList)
         {
-            byte[] buffer = Encoding.UTF8.GetBytes($"{doujin.Uri} ");
-            await fileStream.WriteAsync(buffer).ConfigureAwait(false);
+            var buffer = Encoding.UTF8.GetBytes($"{doujin.Url}\n");
+            fs.Write(buffer);
         }
     }
 
-    /// <summary>
-    /// Gets list of <see cref="Doujin"/>s to download
-    /// </summary>
-    /// <param name="doujins">Object with <see cref="Doujin"/> list</param>
-    /// <returns><see cref="IEnumerable{T}"/> of <see cref="Doujin"/> objects</returns>
-    private static IEnumerable<Doujin> GetDoujinsToDownload(Doujins doujins) =>
-        doujins.DoujinsList.AsParallel().Where(doujin => doujin.Uri != null && !doujin.IsDownloaded);
+    private static IEnumerable<Doujin> GetDoujinsToDownload(IEnumerable<Doujin> doujins) =>
+        doujins.AsParallel().Where(doujin => doujin.Url != null && !doujin.IsDownloaded);
 
     #endregion
 }
